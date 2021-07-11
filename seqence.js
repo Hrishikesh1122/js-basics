3eaf395661cb0770773133d574f114a993cc87df
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-11 20:03:20 +0530

    added : case4 sum & average of 5 two digit ran num

--- seqence.js
+++ seqence.js
@@ -16,3 +16,12 @@ console.log(diceNumber);
 let diceNumber1 = 1+ Math.floor(Math.random()*10)%6;
 let diceNumber2 = 1+ Math.floor(Math.random()*10)%6;
 console.log(diceNumber1 + diceNumber2);
+
+//case 4 : Find sum and average of 5 random 2 digit numbers
+let sum=0;
+for(let i=0;i<5;i++){
+    let ranNumber = Math.floor(Math.random() * 90 + 10);
+    sum+=ranNumber;
+}
+console.log("Sum is :"+sum);
+console.log("Average is : "+sum/5)
