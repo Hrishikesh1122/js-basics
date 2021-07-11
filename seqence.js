7326ae8bc2e0b59dd00dcddc012dc5466f4c5618
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-11 12:40:08 +0530

    added : case 2 get random dice number

--- seqence.js
+++ seqence.js
@@ -7,3 +7,7 @@
 //Case 1 : Use random function to get random sigle digit number
 let number = Math.floor(Math.random()*10);
 console.log(number);
+
+//Case 2 :  Use random to get dice number between 1 to 6 
+let diceNumber = 1+ Math.floor(Math.random()*10)%6;
+console.log(diceNumber);
