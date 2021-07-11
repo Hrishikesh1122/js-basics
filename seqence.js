071f810f38120810ebb657d57bf2268301244e67
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-11 12:34:26 +0530

    added : case1 get single digit random number

---
+++ seqence.js
@@ -0,0 +1,9 @@
+/************************************************************
+ * Sequence practice problems
+ * @author Hrishikesh Ugavekar
+ * @Since 10-07-2021
+ ************************************************************/
+
+//Case 1 : Use random function to get random sigle digit number
+let number = Math.floor(Math.random()*10);
+console.log(number);
