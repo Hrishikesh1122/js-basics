e244ab4fbea44a5ef510b1b3493c32dd31f76049
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-11 12:44:07 +0530

    added : case 3 add two randon dice numbers.

--- seqence.js
+++ seqence.js
@@ -11,3 +11,8 @@ console.log(number);
 //Case 2 :  Use random to get dice number between 1 to 6 
 let diceNumber = 1+ Math.floor(Math.random()*10)%6;
 console.log(diceNumber);
+
+//case 3 : Add two random dice numbers
+let diceNumber1 = 1+ Math.floor(Math.random()*10)%6;
+let diceNumber2 = 1+ Math.floor(Math.random()*10)%6;
+console.log(diceNumber1 + diceNumber2);
